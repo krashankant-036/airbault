5cb614371593353b6fe306132dd2b0871b49d3a5
Abhi <raiabhi@613@gmail.com>
2025-04-07 17:25:03 +0530

    Added new feature to music player

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import './App.css';
 import Home from './component/Home/Home';
 import Main from './component/Main/Main';
-import Sidebar from './component/Sidebar/Sidebar';
+// import Sidebar from './component/Sidebar/Sidebar';
 // import Sidebar from './component/sidebar/Sidebar';
 
 function App() {
